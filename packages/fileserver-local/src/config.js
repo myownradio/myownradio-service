b1fa2ab868c499fdaa3370c13c2a2b7c3658cf07
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2020-02-08 10:33:12 +0200

    refactor: rename environment variables, add fileserver-local to dev environment

--- packages/fileserver-local/src/config.js
+++ packages/fileserver-local/src/config.js
@@ -1,11 +1,19 @@
 const { assert } = require("@myownradio/shared");
 
-const { ROOT_FOLDER, TOKEN_SECRET, PORT } = process.env;
+const {
+  FILESERVER_LOCAL_ROOT_FOLDER,
+  FILESERVER_LOCAL_TOKEN_SECRET,
+  PORT
+} = process.env;
 
-const config = { ROOT_FOLDER, TOKEN_SECRET, PORT };
+const config = {
+  FILESERVER_LOCAL_ROOT_FOLDER,
+  FILESERVER_LOCAL_TOKEN_SECRET,
+  PORT
+};
 
-assert(typeof config.PORT === 'string');
-assert(typeof config.ROOT_FOLDER === 'string');
-assert(typeof config.TOKEN_SECRET === 'string');
+assert(typeof config.PORT === "string");
+assert(typeof config.FILESERVER_LOCAL_ROOT_FOLDER === "string");
+assert(typeof config.FILESERVER_LOCAL_TOKEN_SECRET === "string");
 
 module.exports = config;
--- packages/fileserver-local/src/router/routeHandlers/secure/get.js
+++ packages/fileserver-local/src/router/routeHandlers/secure/get.js
@@ -4,7 +4,7 @@ const { koaHelpers, fsHelpers } = require("@myownradio/shared");
 
 module.exports = function createGetHandler(config) {
   return async ctx => {
-    const filepath = path.join(config.ROOT_FOLDER, ctx.request.path);
+    const filepath = path.join(config.FILESERVER_LOCAL_ROOT_FOLDER, ctx.request.path);
 
     if (!(await fsHelpers.fileExists(filepath))) {
       ctx.throw(404);
