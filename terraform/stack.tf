58092e503bdfcbc736cc03b2722c3c9e23ecdc7d
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2020-02-17 21:20:06 +0200

    ci: attack floating ip to droplet

--- terraform/stack.tf
+++ terraform/stack.tf
@@ -8,27 +8,23 @@ provider "digitalocean" {
   token = var.do_token
 }
 
-resource "digitalocean_floating_ip" "mor_fl" {
-  region = local.digitalocean_region
-}
-
 resource "tls_private_key" "ssh" {
   algorithm = "RSA"
-  rsa_bits  = 4096
+  rsa_bits = 4096
 }
 
 resource "digitalocean_ssh_key" "mor-digitalocean" {
-  name       = "mor-digitalocean"
+  name = "mor-digitalocean"
   public_key = tls_private_key.ssh.public_key_openssh
 }
 
 resource "digitalocean_droplet" "mor_new" {
-  image  = "ubuntu-18-04-x64"
-  name   = "new.myownradio.biz"
+  image = "ubuntu-18-04-x64"
+  name = "new.myownradio.biz"
   region = local.digitalocean_region
-  size   = "s-1vcpu-2gb"
+  size = "s-1vcpu-2gb"
   ssh_keys = [
-  digitalocean_ssh_key.mor-digitalocean.fingerprint]
+    digitalocean_ssh_key.mor-digitalocean.fingerprint]
 
   provisioner "remote-exec" {
     inline = [
@@ -38,11 +34,16 @@ resource "digitalocean_droplet" "mor_new" {
   }
 }
 
+resource "digitalocean_floating_ip" "mor_fl" {
+  region = local.digitalocean_region
+  droplet_id = digitalocean_droplet.mor_new.id
+}
+
 output "mor_new_ip_address" {
   value = digitalocean_floating_ip.mor_fl.ip_address
 }
 
 output "ssh_private" {
-  value     = tls_private_key.ssh.private_key_pem
+  value = tls_private_key.ssh.private_key_pem
   sensitive = true
 }
