3b359c8c72ce7be7d8c188430dfa2434ee14872b
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2020-02-17 13:51:50 +0200

    infra: create floating ip

---
+++ terraform/stack.tf
@@ -0,0 +1,13 @@
+locals {
+  digitalocean_region = "fra1"
+}
+
+variable "do_token" {}
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+resource "digitalocean_floating_ip" "mor_service1" {
+  region              = local.digitalocean_region
+}
