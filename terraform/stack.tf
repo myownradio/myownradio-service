8a7e3d1f4061154a213b7488267923c5b6c6690c
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2020-02-17 21:14:35 +0200

    ci: format terraform files

--- terraform/stack.tf
+++ terraform/stack.tf
@@ -14,20 +14,21 @@ resource "digitalocean_floating_ip" "mor_fl" {
 
 resource "tls_private_key" "ssh" {
   algorithm = "RSA"
-  rsa_bits = 4096
+  rsa_bits  = 4096
 }
 
 resource "digitalocean_ssh_key" "mor-digitalocean" {
-  name = "mor-digitalocean"
+  name       = "mor-digitalocean"
   public_key = tls_private_key.ssh.public_key_openssh
 }
 
 resource "digitalocean_droplet" "mor_new" {
-  image = "ubuntu-18-04-x64"
-  name = "new.myownradio.biz"
+  image  = "ubuntu-18-04-x64"
+  name   = "new.myownradio.biz"
   region = local.digitalocean_region
-  size = "s-1vcpu-2gb"
-  ssh_keys = [digitalocean_ssh_key.mor-digitalocean.fingerprint]
+  size   = "s-1vcpu-2gb"
+  ssh_keys = [
+  digitalocean_ssh_key.mor-digitalocean.fingerprint]
 
   provisioner "remote-exec" {
     inline = [
@@ -42,6 +43,6 @@ output "mor_new_ip_address" {
 }
 
 output "ssh_private" {
-  value = tls_private_key.ssh.private_key_pem
+  value     = tls_private_key.ssh.private_key_pem
   sensitive = true
 }
