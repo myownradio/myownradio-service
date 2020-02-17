a8920ddcef6af3f516748717fd97b259552481b0
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2020-02-17 21:50:46 +0200

    ci: refactoring stack

--- terraform/stack.tf
+++ terraform/stack.tf
@@ -1,5 +1,10 @@
 locals {
   digitalocean_region = "fra1"
+  install_microk8s_inline = [
+    "sudo snap install microk8s --classic",
+    "sudo microk8s.status --wait-ready",
+    "sudo microk8s.enable dns dashboard registry"
+  ]
 }
 
 variable "do_token" {}
@@ -10,21 +15,32 @@ provider "digitalocean" {
 
 resource "tls_private_key" "ssh" {
   algorithm = "RSA"
-  rsa_bits  = 4096
+  rsa_bits = 4096
+}
+
+output "ssh_private" {
+  value = tls_private_key.ssh.private_key_pem
+  sensitive = true
 }
 
 resource "digitalocean_ssh_key" "mor-digitalocean" {
-  name       = "mor-digitalocean"
+  name = "mor-digitalocean"
   public_key = tls_private_key.ssh.public_key_openssh
 }
 
+//resource "digitalocean_floating_ip" "mor_new_fip" {
+//  region = local.digitalocean_region
+//  droplet_id = digitalocean_droplet.mor_new.id
+//}
+
 resource "digitalocean_droplet" "mor_new" {
-  image  = "ubuntu-18-04-x64"
-  name   = "new.myownradio.biz"
+  image = "ubuntu-18-04-x64"
+  name = "new.myownradio.biz"
   region = local.digitalocean_region
-  size   = "s-1vcpu-2gb"
+  size = "s-1vcpu-2gb"
   ssh_keys = [
-  digitalocean_ssh_key.mor-digitalocean.fingerprint]
+    digitalocean_ssh_key.mor-digitalocean.fingerprint
+  ]
 
   connection {
     host = digitalocean_droplet.mor_new.ipv4_address
@@ -34,23 +50,10 @@ resource "digitalocean_droplet" "mor_new" {
   }
 
   provisioner "remote-exec" {
-    inline = [
-      "sudo snap install microk8s --classic",
-      "sudo microk8s.enable dns dashboard registry"
-    ]
+    inline = local.install_microk8s_inline
   }
 }
 
-resource "digitalocean_floating_ip" "mor_fl" {
-  region     = local.digitalocean_region
-  droplet_id = digitalocean_droplet.mor_new.id
-}
-
 output "mor_new_ip_address" {
-  value = digitalocean_floating_ip.mor_fl.ip_address
-}
-
-output "ssh_private" {
-  value     = tls_private_key.ssh.private_key_pem
-  sensitive = true
+  value = digitalocean_droplet.mor_new.ipv4_address
 }
