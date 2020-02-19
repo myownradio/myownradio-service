f74a4d1b20cd8d58bcbe4c4676db9797874909a4
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2020-02-19 22:22:46 +0200

    ci: create master node resource

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
+MASTER_IP_ADDRESS = $(shell cd terraform && terraform output mor_eu_master_ip_address)
+
 install-deployer-ssh-key:
 	(cd terraform && terraform output deployer_private_key) > ~/.ssh/deployer_private_key
+	chmod 0600 ~/.ssh/deployer_private_key
+
+connect-master:
+	ssh -i ~/.ssh/deployer_private_key root@$(MASTER_IP_ADDRESS)
--- terraform/instances.tf
+++ terraform/instances.tf
@@ -3,34 +3,45 @@ resource "digitalocean_ssh_key" "deployer_key" {
   public_key = tls_private_key.deployer_key.public_key_openssh
 }
 
-//resource "digitalocean_droplet" "mor_new" {
-//  image  = "ubuntu-18-04-x64"
-//  name   = "new.myownradio.biz"
-//  size   = "s-1vcpu-2gb"
-//  region = local.digitalocean_region
-//  ssh_keys = [
-//    digitalocean_ssh_key.mor-digitalocean.fingerprint
-//  ]
-//
-//  count = 1
-//
-//  connection {
-//    host        = self.ipv4_address
-//    user        = "root"
-//    type        = "ssh"
-//    private_key = tls_private_key.ssh.private_key_pem
-//  }
-//
-//  provisioner "remote-exec" {
-//    inline = [
-//      "sudo snap install microk8s --classic",
-//      "sudo microk8s.status --wait-ready",
-//      "sudo microk8s.enable dns dashboard registry",
-//      "sudo useradd -m ubuntu"
-//    ]
-//  }
-//}
-//
-//output "mor_new_ip_addresses" {
-//  value = digitalocean_droplet.mor_new.*.ipv4_address
-//}
+resource "digitalocean_droplet" "mor_master" {
+  image  = "ubuntu-18-04-x64"
+  name   = "mor-master"
+  size   = "s-1vcpu-1gb"
+  region = "fra1"
+  ssh_keys = [
+    digitalocean_ssh_key.deployer_key.fingerprint
+  ]
+
+  private_networking = true
+  monitoring         = true
+
+  connection {
+    host        = self.ipv4_address
+    user        = "root"
+    type        = "ssh"
+    private_key = tls_private_key.deployer_key.private_key_pem
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "apt-get update",
+      "apt-get install -y apt-transport-https ca-certificates curl software-properties-common",
+      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -",
+      "add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\"",
+      "apt-get install -y docker-ce",
+      "docker swarm init --advertise-addr=${self.ipv4_address_private}",
+      "mkdir -p /root/swarm",
+      "docker swarm join-token manager > /root/swarm/master-token",
+      "docker swarm join-token worker > /root/swarm/worker-token"
+    ]
+  }
+}
+
+resource "digitalocean_floating_ip" "mor_master" {
+  region     = "fra1"
+  droplet_id = digitalocean_droplet.mor_master.id
+}
+
+output "mor_master_ip_address" {
+  value = digitalocean_floating_ip.mor_master.ip_address
+}
