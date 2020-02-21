51a9901300fb83d8c0b54a1f6301d0858234b018
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2020-02-21 10:21:06 +0200

    terraform fmt

--- terraform/images.tf
+++ terraform/images.tf
@@ -15,6 +15,6 @@ resource "aws_iam_access_key" "deployer_ro" {
 }
 
 resource "aws_iam_user_policy_attachment" "deployer_ro" {
-  user = aws_iam_user.deployer_ro.name
+  user       = aws_iam_user.deployer_ro.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
