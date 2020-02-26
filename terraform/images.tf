a8579f2ac6bbce418b2146246aae2e68d2488d9f
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2020-02-26 18:56:53 +0200

    chore: add image for frontend-proxy

--- terraform/images.tf
+++ terraform/images.tf
@@ -1,11 +1,3 @@
-resource "aws_ecr_repository" "frontend" {
-  name = "myownradio/frontend"
-}
-
-output "frontend_repository_url" {
-  value = aws_ecr_repository.frontend.repository_url
-}
-
 resource "aws_iam_user" "deployer_ro" {
   name = "deployer-ro"
 }
@@ -17,4 +9,20 @@ resource "aws_iam_access_key" "deployer_ro" {
 resource "aws_iam_user_policy_attachment" "deployer_ro" {
   user       = aws_iam_user.deployer_ro.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_ecr_repository" "frontend" {
+  name = "myownradio/frontend"
+}
+
+output "frontend_repository_url" {
+  value = aws_ecr_repository.frontend.repository_url
+}
+
+resource "aws_ecr_repository" "frontend_proxy" {
+  name = "myownradio/frontend-proxy"
+}
+
+output "frontend_proxy_repository_url" {
+  value = aws_ecr_repository.frontend_proxy.repository_url
 }
