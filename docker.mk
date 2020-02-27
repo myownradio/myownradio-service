7c3c1792fead3485a842452306d6267e71e0d542
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2020-02-27 14:31:45 +0200

    chore: cleanup docker.mk

--- docker.mk
+++ docker.mk
@@ -1,12 +1,3 @@
-FRONTEND_PROXY_REPO_URL = $(shell cd terraform && terraform output frontend_proxy_repository_url)
-FRONTEND_REPO_URL = $(shell cd terraform && terraform output frontend_repository_url)
-
-build-frontend-proxy:
-	docker build -t $(FRONTEND_PROXY_REPO_URL) ./services/frontend-proxy
-
-build-frontend:
-	docker build -t $(FRONTEND_REPO_URL) ./services/app/packages/frontend
-
 build-service:
 	docker build -t $(SERVICE) ./services/$(SERVICE)
 
