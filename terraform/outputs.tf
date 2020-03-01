8567d4ee66da52d0546bb2e41f360a0725355c8c
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2020-03-01 17:34:57 +0200

    chore: add comment to commit after successful deploy

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -11,3 +11,7 @@ output "deployer_public_key" {
 output "mor_master_ip_address" {
   value = digitalocean_floating_ip.new.ip_address
 }
+
+output "url" {
+  value = "https://${cloudflare_record.a_record.name}.${cloudflare_zone.myownradio_biz.zone}"
+}
