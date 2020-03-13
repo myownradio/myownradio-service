b56142a497ca933d4b20174e60723e68eadc19b7
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2020-03-13 20:30:22 +0200

    fix: tests

--- app/seeds/02_refresh_tokens.js
+++ app/seeds/02_refresh_tokens.js
@@ -1,12 +1,13 @@
 exports.seed = async function seed(knex) {
+  const now = new Date().toISOString();
   await knex("refresh_tokens").del();
   await knex("refresh_tokens").insert([
     {
       id: 1,
       user_id: 1,
       refresh_token: "8e6112346a91d135e3cb8bbad7f5363eae2108ff",
-      created_at: "2020-02-09T11:12:06.585Z",
-      updated_at: "2020-02-09T11:12:06.585Z"
+      created_at: now,
+      updated_at: now
     },
     {
       id: 2,
