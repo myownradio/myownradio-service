3231f0ad8fbc56d626f624eb215de0e35f301255
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2020-03-14 12:42:03 +0200

    feat(radiomanager): fix suspense

--- app/services/radiomanager-frontend/src/Router.tsx
+++ app/services/radiomanager-frontend/src/Router.tsx
@@ -1,18 +1,21 @@
 import * as React from "react";
+import { Suspense } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 const LoginPage = React.lazy(() => import("./pages/login/LoginPage"));
 
 const Router = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/"><>Home Page</></Route>
-        <Route path="/login">
-          <LoginPage />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <Suspense fallback={<div />}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+          <Route path="/">Home Page</Route>
+        </Switch>
+      </BrowserRouter>
+    </Suspense>
   );
 };
 
