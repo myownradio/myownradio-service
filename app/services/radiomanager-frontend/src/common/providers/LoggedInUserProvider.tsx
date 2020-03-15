8b3504c82e29a15f4f9433b227024a77023297ad
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2020-03-15 14:36:13 +0200

    feat(radiomanager): create user provider with dumb logic

--- app/services/radiomanager-frontend/src/common/providers/LoggedInUserProvider.tsx
+++ app/services/radiomanager-frontend/src/common/providers/LoggedInUserProvider.tsx
@@ -1,12 +1,21 @@
 import * as React from "react";
 import * as PropTypes from "prop-types";
+import { useDependencies } from "../reactAppDependencies";
 
 export interface LoggedInUserProviderProps {
   fallback?: React.ReactElement;
   children?: React.ReactElement;
 }
 
-const LoggedInUserProvider: React.FC<LoggedInUserProviderProps> = ({ children }) => <>{children}</>;
+const LoggedInUserProvider: React.FC<LoggedInUserProviderProps> = ({ fallback, children }) => {
+  const { storageService } = useDependencies();
+
+  if (storageService.get("access_key")) {
+    return <>{children}</>;
+  }
+
+  return <>{fallback}</>;
+};
 
 LoggedInUserProvider.propTypes = {
   fallback: PropTypes.element,
