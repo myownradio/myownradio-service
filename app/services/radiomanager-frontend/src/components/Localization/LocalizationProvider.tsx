2cb615eebd065c5f7aebee6d1904c93497f7c81b
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2020-03-22 17:52:09 +0200

    refactor: move localization logic to localization component

---
+++ app/services/radiomanager-frontend/src/components/Localization/LocalizationProvider.tsx
@@ -0,0 +1,22 @@
+import * as PropTypes from "prop-types";
+import * as React from "react";
+import { IntlProvider } from "react-intl";
+import messagesEn from "~/locales/english";
+
+const LocalizationProvider: React.FC = ({ children }) => {
+  return (
+    <IntlProvider locale="en" messages={messagesEn}>
+      {children}
+    </IntlProvider>
+  );
+};
+
+LocalizationProvider.propTypes = {
+  children: PropTypes.element.isRequired,
+};
+
+LocalizationProvider.defaultProps = {
+  children: null,
+};
+
+export default LocalizationProvider;
