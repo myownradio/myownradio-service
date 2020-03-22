4fc0b60af157b1d61749bffd19617baf8906e4a5
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2020-03-22 16:58:24 +0200

    refactor: split signup form, remove form logic from page

--- app/services/radiomanager-frontend/src/routes/SignupPage/SignupPage.tsx
+++ app/services/radiomanager-frontend/src/routes/SignupPage/SignupPage.tsx
@@ -1,32 +1,12 @@
 import * as React from "react";
-import { useState } from "react";
 import { Redirect } from "react-router-dom";
-import useErrorMessage from "~/components/use/useErrorMessage";
 import { config } from "~/config";
 import LoggedInUserProvider from "~/modules/LoggedInUser/LoggedInUserProvider";
 import SignupForm from "./components/SignupForm";
-import useHandleSubmit from "./use/useHandleSubmit";
 
 const SignupPage: React.FC = () => {
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [errorMessage, setErrorMessage] = useErrorMessage();
-
-  const handleSubmit = useHandleSubmit(email, setEmail, password, setPassword, setErrorMessage);
-
   return (
-    <LoggedInUserProvider
-      fallback={
-        <SignupForm
-          email={email}
-          password={password}
-          onEmailChange={setEmail}
-          onPasswordChange={setPassword}
-          onSignupClicked={handleSubmit}
-          errorMessage={errorMessage}
-        />
-      }
-    >
+    <LoggedInUserProvider fallback={<SignupForm />}>
       <Redirect to={config.routes.home} />
     </LoggedInUserProvider>
   );
---
+++ app/services/radiomanager-frontend/src/routes/SignupPage/components/SignupForm/SignupFormContainer.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import { useState } from "react";
+import useErrorMessage from "~/components/use/useErrorMessage";
+import SignupForm from "./SignupForm";
+import useHandleSubmit from "./use/useHandleSubmit";
+
+const SignupFormContainer: React.FC = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [errorMessage, setErrorMessage] = useErrorMessage();
+
+  const handleSubmit = useHandleSubmit(email, setEmail, password, setPassword, setErrorMessage);
+
+  return (
+    <SignupForm
+      email={email}
+      password={password}
+      onEmailChange={setEmail}
+      onPasswordChange={setPassword}
+      onSignupClicked={handleSubmit}
+      errorMessage={errorMessage}
+    />
+  );
+};
+
+export default SignupFormContainer;
