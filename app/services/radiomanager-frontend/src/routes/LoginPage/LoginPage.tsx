670c408534bfb448616ffa99d44fad136b219049
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2020-03-22 17:01:24 +0200

    refactor: split login form, remove form logic form login page

--- app/services/radiomanager-frontend/src/routes/LoginPage/LoginPage.tsx
+++ app/services/radiomanager-frontend/src/routes/LoginPage/LoginPage.tsx
@@ -1,32 +1,12 @@
 import * as React from "react";
-import { useState } from "react";
 import { Redirect } from "react-router-dom";
-import useErrorMessage from "~/components/use/useErrorMessage";
 import { config } from "~/config";
 import LoggedInUserProvider from "~/modules/LoggedInUser/LoggedInUserProvider";
 import LoginForm from "./components/LoginForm";
-import useHandleSubmit from "./use/useHandleSubmit";
 
 const LoginPage: React.FC = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useErrorMessage();
-
-  const handleSubmit = useHandleSubmit(email, setEmail, password, setPassword, setErrorMessage);
-
   return (
-    <LoggedInUserProvider
-      fallback={
-        <LoginForm
-          email={email}
-          password={password}
-          onEmailChange={setEmail}
-          onPasswordChange={setPassword}
-          onLoginClicked={handleSubmit}
-          errorMessage={errorMessage}
-        />
-      }
-    >
+    <LoggedInUserProvider fallback={<LoginForm />}>
       <Redirect to={config.routes.home} />
     </LoggedInUserProvider>
   );
---
+++ app/services/radiomanager-frontend/src/routes/LoginPage/components/LoginForm/LoginFormContainer.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import { useState } from "react";
+import useErrorMessage from "~/components/use/useErrorMessage";
+import LoginForm from "./LoginForm";
+import useHandleSubmit from "./use/useHandleSubmit";
+
+const LoginFormContainer: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useErrorMessage();
+
+  const handleSubmit = useHandleSubmit(email, setEmail, password, setPassword, setErrorMessage);
+
+  return (
+    <LoginForm
+      email={email}
+      password={password}
+      onEmailChange={setEmail}
+      onPasswordChange={setPassword}
+      onLoginClicked={handleSubmit}
+      errorMessage={errorMessage}
+    />
+  );
+};
+
+export default LoginFormContainer;
