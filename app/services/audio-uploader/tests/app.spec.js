ce647aa519a14e14436504e205a6665181cdc1c1
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2020-03-28 14:45:27 +0200

    feat: update tests with testing metadata signature

--- app/services/audio-uploader/tests/app.spec.js
+++ app/services/audio-uploader/tests/app.spec.js
@@ -2,28 +2,41 @@ const request = require("supertest");
 
 const createApp = require("../src/app");
 const { Config } = require("../src/config");
+const { verifySignatureOfMetadata } = require("../src/utils");
+const withTempDirectory = require("./with/withTempDirectory");
 
 const authenticationToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwZXJtIjpbInVwbG9hZCJdLCJpYXQiOjE1MTYyMzkwMjJ9.6YVvCcjFSaHFw8HgbiUd-sVUQQxmcf8LaNGE7GXIQ6w";
 
+const tempDirectory = withTempDirectory();
+
 let app;
+let config;
+
 beforeEach(() => {
-  const config = new Config({
+  config = new Config({
     AUDIO_UPLOADER_ROOT_DIR: `${__dirname}/__fixtures__/uploadDir`,
     AUDIO_UPLOADER_TOKEN_SECRET: "secret",
+    AUDIO_UPLOADER_METADATA_SECRET: "secret",
+    AUDIO_UPLOADER_TEMP_DIR: tempDirectory,
     PORT: 8080,
   });
 
   app = createApp(config);
 });
 
+afterEach(() => {
+  // todo delete uploads
+});
+
 test("POST /upload - upload new audio file", async () => {
   const filepath = `${__dirname}/__fixtures__/sine.mp3`;
 
-  await request(app.callback())
+  const { headers, text, request: r } = await request(app.callback())
     .post("/upload")
     .set("Authorization", `Bearer ${authenticationToken}`)
     .attach("source", filepath)
     .expect("content-type", /json/)
+    .expect("signature", /.+/)
     .expect(200, {
       name: "sine.mp3",
       hash: "d021bc63dd8f6dee822baa1b2a69b4e9a4d97a7c",
@@ -37,6 +50,8 @@ test("POST /upload - upload new audio file", async () => {
       format: "MP2/3 (MPEG audio layer 2/3)",
       path: "d/0/d021bc63dd8f6dee822baa1b2a69b4e9a4d97a7c",
     });
+
+  expect(verifySignatureOfMetadata(text, headers.signature, config.metadataSecret, 30000)).toBeTruthy();
 });
 
 test("POST /upload - should fail if no file attached", async () => {
