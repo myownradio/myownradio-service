84541dce5be4fee0916760166e96a543ce815f73
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2020-03-30 22:30:36 +0300

    fix: tests

--- app/services/radiomanager-backend/src/router/middleware/requestLogger.ts
+++ app/services/radiomanager-backend/src/router/middleware/requestLogger.ts
@@ -3,14 +3,15 @@ import { Logger } from "winston";
 
 export default function requestLogger(logger: Logger): Middleware {
   return async (ctx: Context, next: () => Promise<void>): Promise<void> => {
-    const route = ctx._matchedRoute;
-    const { method } = ctx.request;
     try {
       await next();
     } catch (error) {
-      const responseCode = error.code || 0;
+      const route = ctx._matchedRoute;
+      const { method } = ctx.request;
+      const responseCode = error.code || -1;
       const errorText = (error.stack || error) as string;
-      logger.warn(`Error happened on inbound http request: ${errorText}`, { method, route, responseCode });
+      logger.warn(`Error happened during inbound http request: ${errorText}`, { method, route, responseCode });
+      throw error;
     }
   };
 }
