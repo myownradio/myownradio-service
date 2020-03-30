6a6734735dfa47b3cc5271b37b2c2483dad11f3f
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2020-03-30 22:55:53 +0300

    refactor: requestLogger.ts

--- app/services/radiomanager-backend/src/router/middleware/requestLogger.ts
+++ app/services/radiomanager-backend/src/router/middleware/requestLogger.ts
@@ -1,6 +1,11 @@
 import { Context, Middleware } from "koa";
 import { Logger } from "winston";
 
+/**
+ * @see https://github.com/koajs/koa/blob/d48d88ee17b780c02123e6d657274cab456e943e/lib/context.js#L150
+ */
+const DEFAULT_KOAJS_ERROR_STATUS_CODE = 500;
+
 export default function requestLogger(logger: Logger): Middleware {
   return async (ctx: Context, next: () => Promise<void>): Promise<void> => {
     try {
@@ -8,9 +13,12 @@ export default function requestLogger(logger: Logger): Middleware {
     } catch (error) {
       const route = ctx._matchedRoute;
       const { method } = ctx.request;
-      const responseCode = error.code || -1;
+
+      const statusCode = error.code || DEFAULT_KOAJS_ERROR_STATUS_CODE;
       const errorText = (error.stack || error) as string;
-      logger.warn(`Error happened during inbound http request: ${errorText}`, { method, route, responseCode });
+
+      logger.warn(`Error happened during inbound http request: ${errorText}`, { method, route, statusCode });
+
       throw error;
     }
   };
