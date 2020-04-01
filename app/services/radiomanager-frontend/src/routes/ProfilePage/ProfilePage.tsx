1ced891dd7ef0529a66a8d07208940a7cae9c9b8
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2020-04-01 09:43:37 +0300

    refactor: cleanup profile page

--- app/services/radiomanager-frontend/src/routes/ProfilePage/ProfilePage.tsx
+++ app/services/radiomanager-frontend/src/routes/ProfilePage/ProfilePage.tsx
@@ -1,28 +1,30 @@
 import * as React from "react";
-import { useCallback } from "react";
+// import { useCallback } from "react";
 
-import { useDependencies } from "~/bootstrap/dependencies";
+// import { useDependencies } from "~/bootstrap/dependencies";
 
 const ProfilePage: React.FC = () => {
-  const { audioUploaderService } = useDependencies();
+  // const { audioUploaderService } = useDependencies();
 
-  const handleClick = useCallback(() => {
-    const input = document.createElement("input");
-    input.setAttribute("type", "file");
-    input.addEventListener("change", (event: Event) => {
-      if (event.target) {
-        const eventTarget = event.target as HTMLInputElement;
-        if (eventTarget.files) {
-          audioUploaderService.uploadAudioFile(eventTarget.files[0]).then(console.log);
-        }
-      }
-    });
-    input.click();
-  }, [audioUploaderService]);
+  // const handleClick = useCallback(() => {
+  //   const input = document.createElement("input");
+  //   input.setAttribute("type", "file");
+  //   input.addEventListener("change", (event: Event) => {
+  //     if (event.target) {
+  //       const eventTarget = event.target as HTMLInputElement;
+  //       if (eventTarget.files) {
+  //         audioUploaderService.uploadAudioFile(eventTarget.files[0]).then(console.log);
+  //       }
+  //     }
+  //   });
+  //   input.click();
+  // }, [audioUploaderService]);
 
   return (
     <section>
-      Upload <button onClick={handleClick}>Me</button>{" "}
+      Choose your radio channel from the list to start editing.
+      <br />
+      <button>Create new radio channel</button>
     </section>
   );
 };
