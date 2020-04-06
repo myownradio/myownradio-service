baaf618e91dbea80ab5fd68ed71b8b17c06bd247
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2020-04-06 19:29:19 +0300

    feat: create API method for fetching audio tracks

---
+++ app/services/radiomanager-backend/src/router/handlers/getRadioChannelTracks.ts
@@ -0,0 +1,38 @@
+import * as knex from "knex";
+import { Context } from "koa";
+import { Config } from "../../config";
+
+export default function getRadioChannelTracks(_: Config, knexConnection: knex) {
+  return async (ctx: Context): Promise<void> => {
+    const { channelId } = ctx.params;
+    const userId = ctx.state.user.uid;
+
+    const channel = await knexConnection
+      .from("radio_channels")
+      .where({ id: channelId })
+      .first();
+
+    if (!channel) {
+      ctx.throw(404);
+    }
+
+    if (channel.user_id !== userId) {
+      ctx.throw(401);
+    }
+
+    const audioTracks = await knexConnection
+      .from("audio_tracks")
+      .where("channel_id", channelId)
+      .orderBy("created_at", "ASC");
+
+    ctx.body = audioTracks.map(audioTrack => ({
+      id: audioTrack.id,
+      name: audioTrack.name,
+      artist: audioTrack.artist,
+      title: audioTrack.title,
+      album: audioTrack.album,
+      bitrate: audioTrack.bitrate,
+      duration: audioTrack.duration,
+    }));
+  };
+}
--- app/services/radiomanager-backend/src/router/index.ts
+++ app/services/radiomanager-backend/src/router/index.ts
@@ -8,6 +8,7 @@ import addTrackToChannel from "./handlers/addTrackToChannel";
 import createChannel from "./handlers/createChannel";
 import getChannel from "./handlers/getChannel";
 import getChannels from "./handlers/getChannels";
+import getRadioChannelTracks from "./handlers/getRadioChannelTracks";
 
 export function createRouter(config: Config, knexConnection: knex, _logger: Logger): Router {
   const router = new Router();
@@ -19,6 +20,7 @@ export function createRouter(config: Config, knexConnection: knex, _logger: Logg
   router.get("/channels", jwtMiddleware, getChannels(config, knexConnection));
   router.post("/channels/create", bodyparser(), jwtMiddleware, createChannel(config, knexConnection));
   router.get("/channels/:channelId", jwtMiddleware, getChannel(config, knexConnection));
+  router.get("/channels/:channelId(\\d+)/tracks", jwtMiddleware, getRadioChannelTracks(config, knexConnection));
   router.post("/channels/:channelId/tracks/add", bodyparser(), jwtMiddleware, addTrackToChannel(config, knexConnection));
 
   return router;
