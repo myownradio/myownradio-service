95413a0fe6067fa03e22f5e9e11fe6114dceec01
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2020-04-12 19:55:49 +0300

    fix(radiomanager-backend): use optional tags

--- app/services/audio-uploader/src/utils.js
+++ app/services/audio-uploader/src/utils.js
@@ -108,10 +108,10 @@ function getMediaFileMetadata(filepath) {
             duration: metadata.format.duration,
             bitrate: metadata.format.bit_rate,
             format: metadata.format.format_long_name || "Unknown audio format",
-            artist: metadata.format.tags.artist || "",
-            title: metadata.format.tags.title || "",
-            album: metadata.format.tags.album || "",
-            genre: metadata.format.tags.genre || "",
+            artist: (metadata.format.tags || {}).artist || "",
+            title: (metadata.format.tags || {}).title || "",
+            album: (metadata.format.tags || {}).album || "",
+            genre: (metadata.format.tags || {}).genre || "",
           });
         }
       });
