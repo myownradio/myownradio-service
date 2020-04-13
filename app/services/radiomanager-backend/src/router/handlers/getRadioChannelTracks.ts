b40243b7fd1aab4b5f7299574c9a26d02d3ae05e
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2020-04-13 20:09:33 +0300

    feat(scheduler): order tracks by order_id

--- app/services/radiomanager-backend/src/router/handlers/getRadioChannelTracks.ts
+++ app/services/radiomanager-backend/src/router/handlers/getRadioChannelTracks.ts
@@ -23,7 +23,7 @@ export default function getRadioChannelTracks(_: Config, knexConnection: knex) {
     const audioTracks = await knexConnection
       .from("audio_tracks")
       .where("channel_id", channelId)
-      .orderBy("created_at", "ASC");
+      .orderBy("order_id", "ASC");
 
     ctx.body = audioTracks.map(audioTrack => ({
       id: audioTrack.id,
