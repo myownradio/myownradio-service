bed4289d4f68ba70b1c822c32784319884bb4294
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2020-04-15 11:52:07 +0300

    feat(entities): create entities package with yet database entities

---
+++ app/libs/entities/db/RefreshTokensEntity.ts
@@ -0,0 +1,11 @@
+import * as t from "io-ts";
+
+export const RefreshTokensEntityContract = t.interface({
+  id: t.number, // integer nullable: NO
+  created_at: t.union([t.null, t.string]), // timestamp with time zone nullable: YES
+  refresh_token: t.string, // character varying nullable: NO
+  user_id: t.number, // integer nullable: NO
+  updated_at: t.union([t.null, t.string]), // timestamp with time zone nullable: YES
+});
+
+export type IRefreshTokensEntity = t.TypeOf<typeof RefreshTokensEntityContract>;
---
+++ app/libs/entities/db/UsersEntity.ts
@@ -0,0 +1,11 @@
+import * as t from "io-ts";
+
+export const UsersEntityContract = t.interface({
+  updated_at: t.union([t.null, t.string]), // timestamp with time zone nullable: YES
+  email: t.string, // character varying nullable: NO
+  password: t.string, // character varying nullable: NO
+  created_at: t.union([t.null, t.string]), // timestamp with time zone nullable: YES
+  id: t.number, // integer nullable: NO
+});
+
+export type IUsersEntity = t.TypeOf<typeof UsersEntityContract>;
