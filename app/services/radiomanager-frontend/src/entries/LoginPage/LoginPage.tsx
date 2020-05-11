76fb0814f33a9a20232102cfcf117470f2a3a282
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2020-05-11 21:22:34 +0300

    feat(radiomanager-frontend): cleanup

--- app/services/radiomanager-frontend/src/entries/LoginPage/LoginPage.tsx
+++ app/services/radiomanager-frontend/src/entries/LoginPage/LoginPage.tsx
@@ -1,5 +1,4 @@
 import React, { ChangeEvent, useCallback, useState } from "react"
-import BlueLayout from "~/layouts/BlueLayout"
 import gettext from "~/utils/gettext"
 import styles from "./LoginPage.scss"
 import { useLogin } from "./use/useLogin"
@@ -19,42 +18,40 @@ export const LoginPage: React.FC<{}> = ({}) => {
   }, [])
 
   return (
-    <BlueLayout>
-      <form className={styles.form} noValidate onSubmit={handleLoginClick}>
-        <h1>Login</h1>
-        <fieldset>
-          <label htmlFor={"email"}>{gettext("Email")}</label>
-          <input
-            disabled={busy}
-            value={email}
-            onChange={handleEmailChange}
-            required
-            name="email"
-            autoComplete="email"
-            type="email"
-            autoFocus
-          />
-        </fieldset>
-        <fieldset>
-          <label htmlFor={"password"}>{gettext("Password")}</label>
-          <input
-            disabled={busy}
-            value={password}
-            onChange={handlePasswordChange}
-            required
-            id="password"
-            name="password"
-            autoComplete="password"
-            type="password"
-          />
-        </fieldset>
-        <fieldset>
-          <button disabled={busy} type={"submit"}>
-            Login
-          </button>
-          {error && <span>{error}</span>}
-        </fieldset>
-      </form>
-    </BlueLayout>
+    <form className={styles.form} noValidate onSubmit={handleLoginClick}>
+      <h1>Login</h1>
+      <fieldset>
+        <label htmlFor={"email"}>{gettext("Email")}</label>
+        <input
+          disabled={busy}
+          value={email}
+          onChange={handleEmailChange}
+          required
+          name="email"
+          autoComplete="email"
+          type="email"
+          autoFocus
+        />
+      </fieldset>
+      <fieldset>
+        <label htmlFor={"password"}>{gettext("Password")}</label>
+        <input
+          disabled={busy}
+          value={password}
+          onChange={handlePasswordChange}
+          required
+          id="password"
+          name="password"
+          autoComplete="password"
+          type="password"
+        />
+      </fieldset>
+      <fieldset>
+        <button disabled={busy} type={"submit"}>
+          Login
+        </button>
+        {error && <span>{error}</span>}
+      </fieldset>
+    </form>
   )
 }
--- app/services/radiomanager-frontend/src/entries/SignupPage/SignupPage.tsx
+++ app/services/radiomanager-frontend/src/entries/SignupPage/SignupPage.tsx
@@ -1,5 +1,4 @@
 import React, { ChangeEvent, useCallback, useState } from "react"
-import BlueLayout from "~/layouts/BlueLayout"
 import gettext from "~/utils/gettext"
 import styles from "./SignupPage.scss"
 import { useSignup } from "./use/useSignup"
@@ -19,42 +18,40 @@ export const SignupPage: React.FC<{}> = ({}) => {
   }, [])
 
   return (
-    <BlueLayout>
-      <form className={styles.form} noValidate onSubmit={handleLoginClick}>
-        <h1>Signup</h1>
-        <fieldset>
-          <label htmlFor={"email"}>{gettext("Email")}</label>
-          <input
-            disabled={busy}
-            value={email}
-            onChange={handleEmailChange}
-            required
-            name="email"
-            autoComplete="email"
-            type="email"
-            autoFocus
-          />
-        </fieldset>
-        <fieldset>
-          <label htmlFor={"password"}>{gettext("Password")}</label>
-          <input
-            disabled={busy}
-            value={password}
-            onChange={handlePasswordChange}
-            required
-            id="password"
-            name="password"
-            autoComplete="password"
-            type="password"
-          />
-        </fieldset>
-        <fieldset>
-          <button disabled={busy} type={"submit"}>
-            Signup
-          </button>
-          {error && <span>{error}</span>}
-        </fieldset>
-      </form>
-    </BlueLayout>
+    <form className={styles.form} noValidate onSubmit={handleLoginClick}>
+      <h1>Signup</h1>
+      <fieldset>
+        <label htmlFor={"email"}>{gettext("Email")}</label>
+        <input
+          disabled={busy}
+          value={email}
+          onChange={handleEmailChange}
+          required
+          name="email"
+          autoComplete="email"
+          type="email"
+          autoFocus
+        />
+      </fieldset>
+      <fieldset>
+        <label htmlFor={"password"}>{gettext("Password")}</label>
+        <input
+          disabled={busy}
+          value={password}
+          onChange={handlePasswordChange}
+          required
+          id="password"
+          name="password"
+          autoComplete="password"
+          type="password"
+        />
+      </fieldset>
+      <fieldset>
+        <button disabled={busy} type={"submit"}>
+          Signup
+        </button>
+        {error && <span>{error}</span>}
+      </fieldset>
+    </form>
   )
 }
