0d2160a5622e7e76f0fe2d3cb22d580f57076c81
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2020-06-07 19:32:07 +0300

    fix: auth-server build

--- app/libs/common/koa.ts
+++ app/libs/common/koa.ts
@@ -1,4 +1,4 @@
-import { Context } from "koa"
+import { Context, DefaultState, Middleware } from "koa"
 
 export interface StrictlyTypedContext<ResBody extends {}> extends Context {
   body: ResBody
@@ -7,3 +7,5 @@ export interface StrictlyTypedContext<ResBody extends {}> extends Context {
 export interface StrictlyTypedRouteHandler<ResBody extends {}> {
   (ctx: StrictlyTypedContext<ResBody>): Promise<void>
 }
+
+export type MiddlewareWithTypedBody<BodyT> = Middleware<DefaultState, { body: BodyT }>
