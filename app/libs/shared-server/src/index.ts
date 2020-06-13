8b1a5a3676a26a0b1c300d553b5927e4b90296b8
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2020-06-13 12:18:06 +0300

    feat: move hashUtils to shared-server package

--- app/libs/shared-server/src/index.ts
+++ app/libs/shared-server/src/index.ts
@@ -1,6 +1,7 @@
 import * as dateUtils from "./dateUtils"
 import * as fsUtils from "./fsUtils"
+import * as hashUtils from "./hashUtils"
 import * as pathUtils from "./pathUtils"
 import * as signature from "./signature"
 
-export { dateUtils, fsUtils, pathUtils, signature }
+export { dateUtils, fsUtils, hashUtils, pathUtils, signature }
