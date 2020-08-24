5c98e2b5a3b892f8bc8d67252c05b07b0f47d551
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2020-08-24 22:31:48 +0300

    feat(radiomanager): fix test

--- app/libs/shared-server/src/pathUtils/convertFileHashToFileUrl.spec.ts
+++ app/libs/shared-server/src/pathUtils/convertFileHashToFileUrl.spec.ts
@@ -1,5 +1,7 @@
 import { convertFileHashToFileUrl } from "./convertFileHashToFileUrl"
 
 test("should convert hash to file url", () => {
-  expect(convertFileHashToFileUrl("12345678", "https://example.com")).toBe("https://example.com/1/2/12345678")
+  expect(convertFileHashToFileUrl("12345678", "file.ext", "https://example.com")).toBe(
+    "https://example.com/1/2/12345678.ext",
+  )
 })
