769bd0bf7d4d332946a2d1340aa53c615e1df8b4
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2020-08-25 20:35:50 +0300

    feat(frontend-proxy): use radiomanager-backend url from config

--- app/services/stream-composer/src/services/RadioManagerClient.ts
+++ app/services/stream-composer/src/services/RadioManagerClient.ts
@@ -2,7 +2,8 @@ import { hashUtils } from "@myownradio/shared-server"
 import { NowPlayingResource } from "@myownradio/shared-types"
 import { AxiosInstance } from "axios"
 import { inject, injectable } from "inversify"
-import { AxiosClientType } from "../di/types"
+import { Config } from "../config"
+import { AxiosClientType, ConfigType } from "../di/types"
 
 export abstract class RadioManagerClient {
   public abstract getNowPlaying(channelId: number): Promise<NowPlayingResource>
@@ -10,12 +11,12 @@ export abstract class RadioManagerClient {
 
 @injectable()
 export class RadioManagerClientImpl implements RadioManagerClient {
-  constructor(@inject(AxiosClientType) private axios: AxiosInstance) {}
+  constructor(@inject(AxiosClientType) private axios: AxiosInstance, @inject(ConfigType) private config: Config) {}
 
   public async getNowPlaying(channelId: number): Promise<NowPlayingResource> {
     const encodedChannelId = hashUtils.encodeId(channelId)
     const response = await this.axios.get<NowPlayingResource>(
-      `https://new.myownradio.biz/api/radiomanager/channels/${encodedChannelId}/now`,
+      `${this.config.radioManagerBackendUrl}/channels/${encodedChannelId}/now`,
     )
 
     return response.data
