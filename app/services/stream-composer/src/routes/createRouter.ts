d22af9a3f2d37dc4440d27002ac3b58acdf2a722
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2020-08-30 13:35:13 +0300

    feat(stream-composer): add more error cases

--- app/services/stream-composer/src/routes/createRouter.ts
+++ app/services/stream-composer/src/routes/createRouter.ts
@@ -2,19 +2,35 @@ import { hashUtils } from "@myownradio/shared-server"
 import { Container } from "inversify"
 import { Context } from "koa"
 import * as Router from "koa-router"
+import { Logger } from "winston"
+import { LoggerType } from "../di/types"
 import { DECODER_CHANNELS, DECODER_FREQUENCY, DECODER_FORMAT, DECODER_CODEC } from "../services/AudioDecoder"
 import { ChannelPlayer } from "../services/ChannelPlayer"
+import { ChannelNotFoundError, RadioManagerClient } from "../services/RadioManagerClient"
 
 export function createRouter(container: Container): Router {
   const router = new Router()
 
   const channelPlayer = container.get(ChannelPlayer)
+  const radioManagerClient = container.get(RadioManagerClient)
+  const logger = container.get<Logger>(LoggerType)
 
   router.get("/listen/:channelId", async (ctx: Context) => {
     const channelId = hashUtils.decodeId(ctx.params.channelId)
 
     if (!channelId) {
       return
+    }
+
+    try {
+      await radioManagerClient.getNowPlaying(channelId)
+    } catch (error) {
+      if (error instanceof ChannelNotFoundError) {
+        ctx.throw(404)
+      }
+
+      logger.error("Error occurred during fetching what is playing on the channel", { reason: error })
+      ctx.throw(503)
     }
 
     ctx.set("pcm-frequency", String(DECODER_FREQUENCY))
